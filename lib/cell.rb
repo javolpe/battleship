0ab075588e370b34abfbcb727cb26f7c662fed1d
Jake Volpe <volpe.jacob@gmail.com>
2020-12-07 20:22:59 -0700

    Finish cell/cell_test  part 1

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,1 +1,25 @@
+require './lib/ship'
 
+class Cell
+  attr_reader :coordinate,
+              :ship
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+  end
+
+
+  def place_ship(boat)
+    @ship = boat
+  end
+
+
+  def empty?
+    if @ship == nil
+      true
+    else
+      false
+    end
+  end
+
+end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -18,4 +18,32 @@ class CellTest < Minitest::Test
     assert_equal cell.coordinate, "B4"
   end
 
+  def test_does_not_contain_a_ship_object
+    cell = Cell.new("B4")
+
+    assert_equal cell.ship, nil
+  end
+
+  def test_does_contain_a_ship_object
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+
+    assert_equal cell.ship, cruiser
+  end
+
+  def test_cell_empty_before_ship_is_added
+    cell = Cell.new("B4")
+
+    assert_equal cell.empty?, true
+  end
+
+  def test_cell_empty_before_ship_is_added
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+
+    assert_equal cell.empty?, false
+  end
+
 end
