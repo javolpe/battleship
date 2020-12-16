d0eb0a4eaaa22e3730fb594a3eb3f1886cef79dd
Regina Casias <reginaannvigil@gmail.com>
2020-12-16 15:12:39 -0700

    Added computer testing

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -6,7 +6,8 @@ require './lib/board'
 require './lib/computer'
 
 class ComputerTest < Minitest::Test
-  def test_player_exists
+
+  def test_computer_exists
     computer = Computer.new("user")
 
     assert_instance_of Computer, computer
@@ -19,4 +20,60 @@ class ComputerTest < Minitest::Test
     assert_equal computer.name, "computer"
     assert_equal computer.board.cells.keys.length, 16
   end
+
+  def test_computer_places_cruiser
+    computer = Computer.new("user")
+    computer.computer_places_cruiser
+    array = []
+    computer.board.cells.keys.each do |key|
+      if computer.board.cells[key].render(true) == "S"
+        array << key
+      end
+      array
+  end
+
+    assert_equal 3, array.count
+  end
+
+  def test_computer_places_submarine
+    computer = Computer.new("user")
+    computer.computer_places_submarine
+    array = []
+    computer.board.cells.keys.each do |key|
+      if computer.board.cells[key].render(true) == "S"
+        array << key
+      end
+      array
+  end
+
+    assert_equal 2, array.count
+  end
+
+  def test_true_computer_has_lost?
+    computer = Computer.new("user")
+    user = Player.new("user")
+    user.board.place(computer.cruiser, ["A1", "A2", "A3"])
+    user.board.place(computer.submarine, ["B1", "B2"])
+    user.board.cells["A1"].fire_upon
+    user.board.cells["A2"].fire_upon
+    user.board.cells["A3"].fire_upon
+    user.board.cells["B1"].fire_upon
+    user.board.cells["B2"].fire_upon
+
+    assert_equal true, computer.has_computer_lost?
+  end
+
+  def test_false_computer_has_lost?
+    computer = Computer.new("user")
+    user = Player.new("user")
+    user.board.place(computer.cruiser, ["A1", "A2", "A3"])
+    user.board.place(computer.submarine, ["B1", "B2"])
+    user.board.cells["A1"].fire_upon
+    user.board.cells["A2"].fire_upon
+    user.board.cells["A3"].fire_upon
+    user.board.cells["B1"].fire_upon
+
+    assert_equal nil, computer.has_computer_lost?
+  end
+
 end
